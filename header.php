47afd747eb11bc684b546cee5c1817bbbcbec96e
OneShinyBo1 <august.abel.vian.bakker@gmail.com>
2022-04-29 10:13:33 +0200

    Php Things added

---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width= , initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/reset.css">
+    <title>PHP project</title>
+</head>
+<body>
+    
+    <nav>
+        <div class="wrapper">
+            <ul>
+                <li><a href="index.php">Home Page</a></li>
+                <li><a href="discover.php">Discover</a></li>
+                <li><a href="blog.php">Blog</a></li>
+                <li><a href="signup.php">Sign Up</a></li>
+                <li><a href="login.php">Log In</a></li>
+            </ul>
+        </div>
+    </nav>    
+    
+    <div class="wrapper">
