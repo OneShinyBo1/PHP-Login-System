8d97ebb5163c5008153f5d011f3bf6d910e045fb
OneShinyBo1 <august.abel.vian.bakker@gmail.com>
2022-04-29 11:36:46 +0200

    Sign Up works

--- signup.php
+++ signup.php
@@ -4,7 +4,7 @@
 
     <section>
         <h2>Sign Up</h2>
-        <form action="signup.inc.php" method="post">
+        <form action="includes/signup.inc.php" method="post">
             <input type="text" name="name" placeholder="Full name...">
             <input type="text" name="email" placeholder="Email...">
             <input type="text" name="uid" placeholder="Username...">
@@ -14,7 +14,7 @@
         </form>
     </section>
 
-    
+
 
 
 <?php
